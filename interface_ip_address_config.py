0b9de9c52e78da65359ebbb08947156c7ba9ccdc
tofonjvd <tofonjavadiandk@gmail.com>
2022-04-25 00:00:54 +0200

    V1.4 add new features

---
+++ interface_ip_address_config.py
@@ -0,0 +1,57 @@
+import csv
+import show_commands_list
+import json
+
+class Cisco_Interface_Ip_Address_Config():
+
+    def device_interface_switchport(ssh_to_device=None, interface=None):
+        command = f"show interfaces switchport"
+        output = ssh_to_device.send_command(command,use_textfsm=True)
+        for each_interface in output:
+            if each_interface['interface'] == interface:
+                if each_interface["switchport"] == "Enabled":
+                    print(f"{each_interface['interface']} is a Layer 2 interface and can not be configured with IP address.")
+                    change_switchport = input("Would you like to disable the Switchport?[yes/no]")
+                    if change_switchport == "yes":
+                        command = [f"interface {interface}", f"no switchport"]
+                        ssh_to_device.send_config_set(command)
+                        flag = True
+                        return flag
+                    break
+                elif each_interface["switchport"] == "Disabled":
+                    pass
+
+
+    def interface_ip_address_config(interfaces_ip_address_config_file=None, ssh_to_device=None, line=None):
+        line_specifier = 0
+        with open(interfaces_ip_address_config_file, mode="r") as interface_ip_file:
+            interface_ip_file_data = csv.reader(interface_ip_file)
+            # For ignoring the first line of our file which contains the headers.
+            interface_ip_file_each_row = next(interface_ip_file_data)
+            # We are setting each index of $interface_ip_file_each_row to the related variable
+            # so we can make our commands
+            while (True):
+                try:
+                    interface_ip_file_each_row = next(interface_ip_file_data)
+                    line_specifier += 1
+                    interface = interface_ip_file_each_row[1]
+                    ip = interface_ip_file_each_row[2]
+                    subnet = interface_ip_file_each_row[3]
+                    shutdown = interface_ip_file_each_row[4]
+                    dhcp = interface_ip_file_each_row[5]
+
+
+                    if line_specifier == line:
+                        Cisco_Interface_Ip_Address_Config.device_interface_switchport(
+                            ssh_to_device=ssh_to_device,
+                            interface=interface)
+                        if dhcp == "yes":
+                            command = [f"interface {interface}", f"ip address dhcp"]
+                        else:
+                            command = [f"interface {interface}", f"ip address {ip} {subnet}"]
+                        ssh_to_device.send_config_set(command)
+                        if shutdown == "no":
+                            command = [f"interface {interface}", f"no shutdown"]
+                        ssh_to_device.send_config_set(command)
+                except StopIteration as error:
+                    break
--- main.py
+++ main.py
@@ -3,15 +3,21 @@ import devices_config
 vlan_file = "path to vlan_automation_file.csv"
 trunk_file = "path to trunk_automation_file.csv"
 devices_file = "path to devices_list.csv"
+interface_ip_address_file = "path to interfaces_ip_address_config_file.csv"
+
+start = time.time()
 
 obj_device = devices_config.Cisco_IOS_Switch
 obj_make_list_of_devices = devices_config.Cisco_IOS_Switch.make_devices(devices_file_path=devices_file)
 
+obj_device.devices_to_config(self=devices_config.Cisco_IOS_Switch,all_devices=obj_make_list_of_devices,config_file_path=interface_ip_address_file,cfg="int_ip_config")
+
 obj_show_data = show_commands_list.Show_Commands.commands_list(all_devices=obj_make_list_of_devices)
+# obj_device.devices_to_config(self=devices_config.Cisco_IOS_Switch,all_devices=obj_make_list_of_devices,config_file_path=vlan_file,cfg="vlan")
+# obj_device.devices_to_config(self=devices_config.Cisco_IOS_Switch,all_devices=obj_make_list_of_devices,config_file_path=vlan_file,cfg="int_vlan_config")
+# obj_device.devices_to_config(self=devices_config.Cisco_IOS_Switch,all_devices=obj_make_list_of_devices,config_file_path=trunk_file,cfg="int_trunk_config")
 
-obj_device.devices_to_config(self=devices_config.Cisco_IOS_Switch,all_devices=obj_make_list_of_devices,config_file_path=vlan_file,cfg="vlan")
-obj_device.devices_to_config(self=devices_config.Cisco_IOS_Switch,all_devices=obj_make_list_of_devices,config_file_path=vlan_file,cfg="int_vlan_config")
-obj_device.devices_to_config(self=devices_config.Cisco_IOS_Switch,all_devices=obj_make_list_of_devices,config_file_path=trunk_file,cfg="int_trunk_config")
-
+end = time.time()
 
 print("\nDONE !!!")
+print(f"time: {end-start}")
